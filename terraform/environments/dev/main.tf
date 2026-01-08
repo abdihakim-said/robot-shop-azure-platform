a14729e3e7253c3ab1ec894f12fa10334708afcb
Abdihakim-said <abdihakimsaid1@gmail.com>
2026-01-08 08:52:02 +0300

    fix: use hardcoded environment value in user node pool
    - Replace var.environment with 'dev' since variable not declared
    - Fixes terraform validation error for user node pool

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -146,7 +146,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "user" {
   # Labels to identify user nodes
   node_labels = {
     "nodepool-type" = "user"
-    "environment"   = var.environment
+    "environment"   = "dev"
     "workload"      = "applications"
   }
 
